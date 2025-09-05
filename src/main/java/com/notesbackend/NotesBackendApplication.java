e5ef051b86151c93d4d7620fa2002ec5fa792324
MarellaHasmitha <hasmithamarella02@gmail.com>
2025-09-05 21:15:18 +0530

    Update NotesBackendApplication.java

--- src/main/java/com/notesbackend/NotesBackendApplication.java
+++ src/main/java/com/notesbackend/NotesBackendApplication.java
@@ -2,6 +2,10 @@ package com.notesbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class NotesBackendApplication {
@@ -9,5 +13,22 @@ public class NotesBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotesBackendApplication.class, args);
 	}
+
+	@Configuration
+	public class WebConfig {
+	    @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**") // allow all endpoints
+	                        .allowedOrigins("http://localhost:3000") // frontend URL
+	                        .allowedMethods("*")
+	                        .allowedHeaders("*");
+	            }
+	        };
+	    }
+	}
+
 
 }
